b4d2b15730c9b6f8918adf051ee9b877750c9cec
Ammar Ekbote <ammar.ekbote@lacework.net>
2023-08-07 22:54:29 +0000

    add permission to query machine type from scanning project

--- custom_roles.tf
+++ custom_roles.tf
@@ -66,6 +66,7 @@ resource "google_project_iam_custom_role" "agentless_orchestrate" {
     "compute.instances.list",
     "compute.instances.setMetadata",
     "compute.instances.setServiceAccount",
+    "compute.machineTypes.get",
     "compute.snapshots.create",
     "compute.snapshots.delete",
     "compute.snapshots.list",
