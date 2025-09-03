76f6b75231ed0a56d184e5a956a00fcbc39f954f
Joe Wilder <joe.wilder@lacework.net>
2025-09-03 17:59:34 +0000

    fix: only enforce org_id check for org level integrations

--- checks.tf
+++ checks.tf
@@ -3,7 +3,7 @@ check "non_empty_organization_id" {
   // in a folder. In this case, google_project.selected[0].org_id will be empty whereas google_project.selected[0].folder_id
   // will be non-empty. We'd need to ask the user to provide the organization_id in such cases.
   assert {
-    condition     = local.organization_id != "" && local.integration_type == "ORGANIZATION"
+    condition     = var.organization_id != "" || var.integration_type == "PROJECT"
     error_message = "No `organization_id` is provided and we failed to derive one. Please provide `organization_id`."
   }
 }
